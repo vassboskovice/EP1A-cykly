31ea5381b5c236efeb09114dcc0550eed65e8aea
Jiří Hlaváček <jirka@jhlavacek.cz>
2024-05-06 13:22:18 +0200

    for - next

--- EP1A-cykly/Program.vb
+++ EP1A-cykly/Program.vb
@@ -3,14 +3,12 @@ Imports System
 Module Program
   Sub Main(args As String())
     Dim cislo As Integer
-    Dim pocet As Integer = 0
 
-    Do
+    For i = 1 To 5
       Console.Write("Zadej èíslo: ")
       cislo = Console.ReadLine
-      pocet = pocet + 1
-      Console.WriteLine($"{pocet}. zadané èíslo je {cislo}")
-    Loop While pocet < 5
+      Console.WriteLine($"{i}. zadané èíslo je {cislo}")
+    Next
 
   End Sub
 End Module

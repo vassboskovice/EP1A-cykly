50fed1560bee767b8df82071197f1dacd3d17b7a
Jiří Hlaváček <jirka@jhlavacek.cz>
2024-05-06 13:31:02 +0200

    součet čísel

--- EP1A-cykly/Program.vb
+++ EP1A-cykly/Program.vb
@@ -3,12 +3,16 @@ Imports System
 Module Program
   Sub Main(args As String())
     Dim cislo As Integer
+    Dim soucet As Integer = 0
 
-    For i = 1 To 5
-      Console.Write("Zadej èíslo: ")
-      cislo = Console.ReadLine
-      Console.WriteLine($"{i}. zadané èíslo je {cislo}")
+    Console.Write("Zadej èíslo: ")
+    cislo = Console.ReadLine
+
+    For i = 1 To cislo
+      soucet = soucet + i
     Next
+
+    Console.WriteLine($"Souèet od 1 do {cislo} je {soucet}")
 
   End Sub
 End Module
